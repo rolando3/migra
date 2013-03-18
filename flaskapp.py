b0eab9112f10435bbfba79180d39b0ef005509a6
Roalndo <rdeaguiar@yahoo.com>
2013-03-18 18:46:30 -0400

    Fix startup

--- flaskapp.py
+++ flaskapp.py
@@ -69,4 +69,5 @@ def cache():
     return migra.cache(request.form['data'])
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
