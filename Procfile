c1ed7916a3b68186d7b79ee66e36d77835c4c34b
Roalndo <rdeaguiar@yahoo.com>
2013-03-20 18:31:41 -0400

    Add clock process

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: python flaskapp.py
-
+clock: python clock.py
---
+++ clock.py
@@ -0,0 +1,16 @@
+from apscheduler.scheduler import Scheduler
+
+sched = Scheduler()
+
+@sched.interval_schedule(minutes=)
+def timed_job():
+    print 'This job is run every three minutes.'
+
+#@sched.cron_schedule(day_of_week='mon-fri', hour=17)
+#def scheduled_job():
+#    print 'This job is run every weekday at 5pm.'
+
+sched.start()
+
+while True:
+    pass
